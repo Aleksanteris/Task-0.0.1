dbcee856eedf80fcef2aba0ea6e56e983186d051
Aliaksandr <aleksanteri.s111@gmail.com>
2018-06-02 09:13:56 +0300

    initialcommit

---
+++ Block/Adminhtml/BlockClassName.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VendorName\ModuleName\Block\Adminhtml;
+
+class BlockClassName extends \Magento\Framework\View\Element\Template
+{
+    public function getBlockResult()
+    {
+        return 'I do it ! :)';
+    }
+}
